e15ba394415b900b5151d2b9bbe4d8ce9c7de461
Brandon_J_Allison <you@example.com>
2019-03-18 17:33:50 -0400

    MVP met with some styling to be done

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (<div className ="footerDiv">
+            <i className="far fa-comment"></i>
+            <i className="fas fa-sync-alt"></i>
+            <i className="far fa-heart"></i>
+            <i className="far fa-envelope"></i>
+        </div>)
+};
+
+export default Footer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail.js';
+import HeaderTitle from './HeaderTitle.js';
+import HeaderContent from './HeaderContent.js';
+
+const HeaderContainter = () => {
+    return <div className="headerContainer">
+     
+     <ImageThumbnail />
+        <div className= "header">
+            
+            <HeaderTitle />
+            <HeaderContent />
+        </div>
+        
+        
+    </div>
+    
+}
+export default HeaderContainter;
